dc25731ce3440996b4bc365e94544bb202d57ad3
erik-ferreira <erikdesouzagf@gmail.com>
2023-10-03 16:44:45 -0300

    install depts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { StatusBar } from "expo-status-bar"
-import { Image, Text, View } from "react-native"
+import * as ImagePicker from "expo-image-picker"
+import { ActivityIndicator, Image, Alert, View } from "react-native"
 
 import { Button } from "./components/Button"
 
@@ -10,7 +11,34 @@ const placeholderImg =
   "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=170667a&w=0&k=20&c=Q7gLG-xfScdlTlPGFohllqpNqpxsU1jy8feD_fob87U="
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false)
   const [selectedImageUri, setSelectedImageUri] = useState("")
+
+  async function handleSelectImage() {
+    setIsLoading(true)
+
+    try {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+      if (status !== "granted") {
+        Alert.alert("Desculpe, precisamos acessar a galeria para prosseguir")
+        return
+      }
+
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+      })
+
+      if (!result.canceled) {
+        setSelectedImageUri(result.assets[0].uri)
+      }
+    } catch (error) {
+      console.log("error", error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -23,9 +51,13 @@ export default function App() {
         }}
       />
 
-      <View style={styles.results}></View>
+      <View style={styles.results} />
 
-      <Button title="Selecionar image" />
+      {isLoading ? (
+        <ActivityIndicator color="#5F1FBB" />
+      ) : (
+        <Button title="Selecionar image" onPress={handleSelectImage} />
+      )}
     </View>
   )
 }
